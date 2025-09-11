49ea8cd60d16cbb5fedaf9f0736d6d0fc90796c4
Sergio <serjimenez940@gmail.com>
2025-09-11 12:55:06 -0500

    feat: agregar boton en la pantalla principal

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab04.ui.theme.Lab04Theme
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,13 +31,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    androidx.compose.foundation.layout.Column(modifier = modifier) {
+        Text(text = "Hello $name!")
+        Button(onClick = { /* TODO: acción futura */ }) {
+            Text("Navegar")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
